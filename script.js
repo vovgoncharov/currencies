0a99626f038ad382494676666bb70451a56dddc9
Volodymyr Honcharov <deykdog@gmail.com>
2021-11-11 09:04:01 +0200

    finish project

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 "use strict";
+let cards = document.querySelectorAll(".card");
+const getCardActive = () => {
+    cards.forEach((data) => {
+        data.addEventListener("click", (e) => {
+            e.currentTarget.classList.toggle("active");
+        });
+    });
+};
 
+getCardActive();
